be68a5e9e56960c1fc3d586e20146798881b77ca
Shaddy <shadybett540@gmail.com>
2023-10-08 10:16:51 +0300

    completed

--- index.js
+++ index.js
@@ -1,25 +1,37 @@
-function isPalindrome(word) {
+
   // Write your algorithm here
-}
+  function isPalindrome(word) {
+    // Convert the word to lowercase to handle case-insensitivity
+    word = word.toLowerCase();
+  
+    // Initialize pointers
+    let leftIndex = 0;
+    let rightIndex = word.length - 1;
+  
+    // Use a while loop to check characters from both ends of the word
+    while (leftIndex < rightIndex) {
+      // If characters at leftIndex and rightIndex are not equal, it's not a palindrome
+      if (word[leftIndex] !== word[rightIndex]) {
+        return false;
+      }
+      leftIndex++;
+      rightIndex--;
+    }
+  
+    // If the loop completes without returning false, it's a palindrome
+    return true;
+  }
+  
+  // Example usage and testing
+  if (require.main === module) {
+    console.log("Expecting: true");
+    console.log("=>", isPalindrome("racecar"));
+  
+    console.log("");
+  
+    console.log("Expecting: false");
+    console.log("=>", isPalindrome("robot"));
+  }
+  
+  module.exports = isPalindrome;
 
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
-if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
-
-  console.log("");
-
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
-}
-
-module.exports = isPalindrome;
